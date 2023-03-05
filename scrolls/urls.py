47f00168e35aa7761a173848766cfe97bc0d7940
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-03-06 01:18:01 +0530

    setup folder structure

--- scrolls/urls.py
+++ scrolls/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('team/', include('team.urls')),
+    path('participants/', include('participants.urls')),
 ]
