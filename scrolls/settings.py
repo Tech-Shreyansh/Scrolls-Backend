18b8b796f6d00a4ce4c0bd20700fdc6032ce6d42
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-03-11 15:24:05 +0530

    static files set

--- scrolls/settings.py
+++ scrolls/settings.py
@@ -42,16 +42,19 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'participants.apps.ParticipantsConfig',
+    'whitenoise.runserver_nostatic', 
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
 ]
 
 AUTH_USER_MODEL = 'participants.Participant'
@@ -126,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
