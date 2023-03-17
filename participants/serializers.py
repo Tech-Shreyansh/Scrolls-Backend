7a4548927bd8386a095e4c7c74bbe344f61b7d76
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-03-17 14:47:28 +0530

    added referral code in response

--- participants/serializers.py
+++ participants/serializers.py
@@ -5,7 +5,7 @@ from .models import *
 class participant_serializer(ModelSerializer):
     class Meta:
         model = Participant
-        fields = ["id","name", "email", "password", "gender", "college", "course", "branch", "year_of_study","mobile","member_id"]
+        fields = ["id","name", "email", "password", "gender", "college", "course", "branch", "year_of_study","mobile","member_id","referral_code","referral_count"]
         extra_kwargs={
             'password':{'write_only': True},
         }
