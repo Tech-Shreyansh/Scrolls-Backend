d1aac389b9a086351d14fadabb484ec83348fb79
yash114bansal <yash114bansal@gmail.com>
2024-05-03 20:37:12 +0530

    add filters on admin panel

--- participants/admin.py
+++ participants/admin.py
@@ -2,10 +2,43 @@ from django.contrib import admin
 from .models import *
 from import_export.admin import ExportActionMixin
 
+class SynopsisSubmittedFilter(admin.SimpleListFilter):
+    title = 'Synopsis Submitted'
+    parameter_name = 'has_synopsis'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.exclude(synopsis='')
+        elif self.value() == 'no':
+            return queryset.filter(synopsis='')
+
+class PaperSubmittedFilter(admin.SimpleListFilter):
+    title = 'Technical Paper Submitted'
+    parameter_name = 'has_paper'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Yes'),
+            ('no', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.exclude(paper='')
+        elif self.value() == 'no':
+            return queryset.filter(paper='')
+
 class Team_admin(ExportActionMixin, admin.ModelAdmin):
-    search_fields = ('domain', "topic", "team_id")
-    list_display = ['name', "domain", "topic", "team_id", 'has_submitted_synopsis', 'has_submitted_paper']
+    search_fields = ('name','domain', "topic", "team_id")
+    list_display = ['name', "domain", "topic", "team_id","is_selected" , 'has_submitted_synopsis', 'has_submitted_paper']
     exclude = ('password',)
+    list_filter = (SynopsisSubmittedFilter,PaperSubmittedFilter, "is_selected", "domain")
 
     def has_submitted_synopsis(self, obj):
         return obj.synopsis.name != ""
@@ -13,11 +46,13 @@ class Team_admin(ExportActionMixin, admin.ModelAdmin):
     def has_submitted_paper(self, obj):
         return obj.paper.name != ""
 
+    has_submitted_synopsis.boolean = True
+    has_submitted_paper.boolean = True
     has_submitted_synopsis.short_description = 'Synopsis Submitted'
     has_submitted_paper.short_description = 'Technical Paper Submitted'
 
 class Participant_admin(ExportActionMixin, admin.ModelAdmin):
-    search_fields = ('gender', "course", "year_of_study", "is_ambassador")
+    search_fields = ('name', 'gender', "course", "year_of_study", "is_ambassador")
     list_display = ['email','name','gender', "course", "year_of_study", "college","member_id"]
     exclude = ('password',)
 
