f014a2757a317101fd9527c94dc0395ea9931a9a
LAKSHAY GUPTA <99826011+ilakshaygupt@users.noreply.github.com>
2024-05-03 13:25:33 +0530

    added Status Filters for Team Synopsis and Technical Paper Submission

--- participants/admin.py
+++ participants/admin.py
@@ -3,9 +3,18 @@ from .models import *
 from import_export.admin import ExportActionMixin
 
 class Team_admin(ExportActionMixin, admin.ModelAdmin):
-    search_fields = ('domain', "topic","team_id")
-    list_display = ['name',"domain","topic","team_id"]
+    search_fields = ('domain', "topic", "team_id")
+    list_display = ['name', "domain", "topic", "team_id", 'has_submitted_synopsis', 'has_submitted_paper']
     exclude = ('password',)
+
+    def has_submitted_synopsis(self, obj):
+        return obj.synopsis.name != ""
+
+    def has_submitted_paper(self, obj):
+        return obj.paper.name != ""
+
+    has_submitted_synopsis.short_description = 'Synopsis Submitted'
+    has_submitted_paper.short_description = 'Technical Paper Submitted'
 
 class Participant_admin(ExportActionMixin, admin.ModelAdmin):
     search_fields = ('gender', "course", "year_of_study", "is_ambassador")
